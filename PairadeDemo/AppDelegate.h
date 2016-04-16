60985722c2be35298e27e4f8173dfccd0bc7cbcd
Josh Wright <joshuawright11@gmail.com>
2016-04-16 15:46:22 -0500

    Initial Commit

---
+++ PairadeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PairadeDemo
+//
+//  Created by Josh Wright on 4/16/16.
+//  Copyright © 2016 Pairade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
